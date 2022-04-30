ed6f93944fe3a8d3ada0166c56d615dd6faf1294
liorelisberg <liorelisberg@gmail.com>
2022-04-30 16:32:55 +0300

    added logger object, moved windows to folder

--- src/GuiDriver.py
+++ src/GuiDriver.py
@@ -1,81 +1,21 @@
-import os
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager
+
 from kivy.core.window import Window
 from kivy.lang import Builder
-from components.popups.popup import PopUp
-from components.validators.validate_url import UrlValidator
+
+from windows import YouTube 
+from windows import MyPC
+from windows import MainWindow
+from windows import AboutUs
+
+from logs.LoggerFactory import Logger
+
 import webbrowser
-from pytube import YouTube as YT
-from pytube.cli import on_progress
 
 Builder.load_file("myapp.kv")
 Window.clearcolor = (1,1,1,1)
-class MainWindow(Screen):
-    pass
-
-class YouTube(Screen):
-    urlValidator = UrlValidator()
-    _video_output_path = "venv/Video_Downloads"
-    _subtitle_output_path = "venv/Video_Downloads"
-        
-    def create_popup(self,url):
-        if(self.urlValidator.is_empty_url(url)):
-            PopUp("Invalid URL","Received Empty URL")
-            
-        elif(self.urlValidator.is_valid_url(url)):
-            self.download_video(url)
-
-            self.url.text = "" 
-              
-        else:
-            PopUp("Invalid Url","URL is invalid")    
-            self.url.text = ""
-
-    def submit_url(self):
-        url = self.url.text
-        self.create_popup(url)
-    
-    def download_complete(self,stream,file_path):
-        file_name = os.path.basename(file_path).split('.')[0]
-        PopUp("Success","Successfuly Downloaded\n"+file_name)
-        print("Done !")
-        
-        
-    def download_video(self, url):
-        yt = YT(url,on_progress_callback=on_progress,on_complete_callback=self.download_complete)
-        # print(yt.captions)
-        title = yt.title
-        # print("captions: ",yt.captions)
-        if len(yt.captions) != 0 :
-            caption = yt.captions['a.en']
-            # print("caption: ",caption)
-            subtitle = caption.generate_srt_captions()
-            # print("subtitle: ",subtitle)
-            completeName = os.path.join("Subtitles_Files", title + '.srt')
-            open(completeName, 'w').write(subtitle)
-        
-        # print("title: ",title,"caption size: ", caption)
-        print("downloading ...")
-
-        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(self._video_output_path)
-        
-    
-class MyPC(Screen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
-        Window.bind(on_drop_file=MyPC.handlesDrop)
-        
-    def select(self, *args):
-        try: self.label.text = args[1][0]
-        except: pass
-        
-    def handlesDrop(self,filename,x,y):
-        self.file_name = filename.decode("utf-8") 
-        PopUp(title_text="Success",msg_text=f'this is my dropped file:\n{self.file_name}')
-        
-class AboutUs(Screen):
-    pass
+Logger = Logger()
             
 sm = ScreenManager()
 sm.add_widget(MainWindow(name='main'))
@@ -90,4 +30,6 @@ class MyApp(App):
         return sm
 
 if __name__ == "__main__":
+    logger = Logger.get_logger()
+    logger.info("Initializing Application")
     MyApp().run()
---
+++ src/windows/YouTube.py
@@ -0,0 +1,57 @@
+import os
+
+from kivy.uix.screenmanager import Screen
+
+from pytube import YouTube as YT
+from pytube.cli import on_progress
+
+from components.validators.validate_url import UrlValidator
+from components.popups.popup import PopUp
+
+
+class YouTube(Screen):
+    urlValidator = UrlValidator()
+    _video_output_path = "/Video_Downloads"
+    _subtitle_output_path = "/Subtitles_Files"
+        
+    def create_popup(self,url):
+        if(self.urlValidator.is_empty_url(url)):
+            PopUp("Invalid URL","Received Empty URL")
+            
+        elif(self.urlValidator.is_valid_url(url)):
+            self.download_video(url)
+
+            self.url.text = "" 
+              
+        else:
+            PopUp("Invalid Url","URL is invalid")    
+            self.url.text = ""
+
+    def submit_url(self):
+        url = self.url.text
+        self.create_popup(url)
+    
+    def download_complete(self,stream,file_path):
+        file_name = os.path.basename(file_path).split('.')[0]
+        PopUp("Success","Successfuly Downloaded\n"+file_name)
+        print("Done !")
+        
+        
+    def download_video(self, url):
+        yt = YT(url,on_progress_callback=on_progress,on_complete_callback=self.download_complete)
+        # print(yt.captions)
+        title = yt.title
+        # print("captions: ",yt.captions)
+        if len(yt.captions) != 0 :
+            caption = yt.captions['a.en']
+            # print("caption: ",caption)
+            subtitle = caption.generate_srt_captions()
+            # print("subtitle: ",subtitle)
+            completeName = os.path.join("Subtitles_Files", title + '.srt')
+            open(completeName, 'w').write(subtitle)
+        
+        # print("title: ",title,"caption size: ", caption)
+        print("downloading ...")
+
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(self._video_output_path)
+   
