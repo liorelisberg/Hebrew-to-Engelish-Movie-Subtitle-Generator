cc7e562e2508c59edaf93064fd3b3c8da4a3d04f
liorelisberg <liorelisberg@gmail.com>
2022-06-14 23:34:28 +0300

    creating progress bar

--- src/progressbar_example/progress.py
+++ src/progressbar_example/progress.py
@@ -1,3 +1,4 @@
+import time
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
@@ -6,19 +7,24 @@ from kivy.lang import Builder
 Builder.load_file('progress.kv')
 
 class MyLayout(Widget):
+    
 	def press_it(self):
-		# Grab the current progress bar value
-		current = self.ids.my_progress_bar.value
-		# If statement to start over after 100
-		if current == 1:
-			current = 0
+            for i in range(10):
+                time.sleep(1.5)
+                # Grab the current progress bar value
+                current = self.ids.my_progress_bar.value
+                # If statement to start over after 100
+                if current == 1:
+                    current = 0
 
-		# Increment value by .25
-		current += .25
-		# Update the progress bar
-		self.ids.my_progress_bar.value = current
-		# Update the label
-		self.ids.my_label.text = f'{int(current*100)}% Progress'
+                # Increment value by .25
+                current += .25
+                # Update the progress bar
+                self.ids.my_progress_bar.value = current
+                # Update the label
+                self.ids.my_label.text = f'{int(current*100)}% Progress'
+                print(f'{int(current*100)}% Progress')
+
 
 class AwesomeApp(App):
 	def build(self):
