56c9ff231142f8d9763ea82e57b0a725eba5ce43
Lior Elisberg <liorelisberg@gmail.com>
2022-11-02 14:10:17 +0200

    Add files via upload (#8)

--- src/GuiDriver.py
+++ src/GuiDriver.py
@@ -1,19 +1,11 @@
-import logging
-
-from logs.LoggerFactory import Logger
-logger = Logger().get_logger(__name__)
-logger.info("initiliaizing project")
-
-from kivy.logger import Logger
-logging.Logger.manager.root = Logger
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from kivy.core.window import Window
 from kivy.lang import Builder
+# from AI.Model.Translator import Translator
 
-from src.windows.MainWindow import MainWindow
+from windows.MainWindow import MainWindow
 from windows.YouTube import YouTube 
 from windows.MyPC import MyPC
 from windows.AboutUs import AboutUs
@@ -21,19 +13,27 @@ from windows.AboutUs import AboutUs
 Window.clearcolor = (1,1,1,1)
 Builder.load_file('app.kv')
             
-sm = ScreenManager()
-sm.add_widget(MainWindow(name='main'))
-sm.add_widget(YouTube(name='YouTube'))
-sm.add_widget(MyPC(name='MyPC'))
-sm.add_widget(AboutUs(name='AbutUs'))
+# sm = ScreenManager()
+# sm.add_widget(MainWindow(name='Main'))
+# sm.add_widget(YouTube(name='YouTube'))
+# sm.add_widget(MyPC(name='MyPC'))
+# sm.add_widget(AboutUs(name='AboutUs'))
 
 class MyApp(App):
     def build(self):
-        logger.info("initializing window")
         self.title = 'Hebrew Subtitles Maker'
-        sm.current = 'main'
+        
+        # translator = Translator()
+        
+        sm = ScreenManager()
+        sm.add_widget(MainWindow(name='Main'))
+        sm.add_widget(YouTube(name='YouTube'))
+        sm.add_widget(MyPC(name='MyPC'))
+        sm.add_widget(AboutUs(name='AboutUs'))
+        
+        sm.current = 'Main'
+        
         return sm
 
 if __name__ == "__main__":
-    logger.info("MyApp is running")    
     MyApp().run()
--- src/windows/MainWindow.py
+++ src/windows/MainWindow.py
@@ -1,6 +1,4 @@
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager,Screen
-
+from kivy.uix.screenmanager import Screen
 class MainWindow(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
